4f45947d131856177ee5a91d9d1e82d8c9ac69a8
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2026-07-26 13:33:48 +0800

    Route foreign libgcov through xgcc assembly

--- core-packages/gcc/package.sh
+++ core-packages/gcc/package.sh
@@ -656,7 +656,7 @@ if [[ -x "\$xgcc" ]]; then
 
     if [[ "\$source_basename" == "libgcc2.c" || "\$source_basename" == "sfp-exceptions.c" || "\$source_input" == */soft-fp/* ]]; then
       libgcc_stage1=preprocess
-    elif [[ "\$source_basename" == "strub.c" ]]; then
+    elif [[ "\$source_basename" == "strub.c" || "\$source_basename" == "libgcov-profiler.c" || "\$source_basename" == "libgcov-interface.c" ]]; then
       libgcc_stage1=assembly
     fi
 
